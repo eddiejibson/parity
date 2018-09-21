115101c059fb5c4bbd5f304df0394e80057a7524
Eddie Jibson <jibson@tuta.io>
2018-09-21 09:21:34 +0100

    Update parity.js

--- parity.js
+++ parity.js
@@ -1,36 +1,41 @@
+const isOdd = ((val) => {
+  if (val %2 == 0) {
+    return false;
+  }  else {
+    return true;
+  }
+});
 
-  
-  const isOdd = ((val) => {
-    if (val %2 == 0) {
-      return false;
-    }  else {
-      return true;
-    }
-  });
-  
-  var getOutput = (() => {
-    let userInput = document.getElementById('binaryInput').value,
-        oneCount = 0;
-    if (userInput.length == 7) {
-    for (i = 0; i < userInput.length; i++) { 
-      console.log(userInput.charAt(i));
-      if (userInput.charAt(i) == "1") {
-        oneCount++;
-      }
-    }
-    if (isOdd(oneCount)) {
-      console.log("Parity bit is 1");
-      swal({
-        icon: "success",
-        text: `1${userInput}` 
-      });
-      //parity bit is 1
-    } else {
-      //parity bit is 0
+const isBinaryOdd((val) => {
+  for (i = 0; i < val.length; i++) { 
+    if (userInput.charAt(i) == "1") {
+      oneCount++;
     }
   }
-  });
-  document.getElementById("submit").addEventListener("click", getOutput);
+  if (oneCount %2 == 0) {
+    return false;
+  }  else {
+    return true;
+  }
+});
+  
+var getOutput = (() => {
+let userInput = document.getElementById('binaryInput').value,
+oneCount = 0;
+if (userInput.length == 7) {
+  if (isBinaryOdd(userInput)) {
+    console.log("Parity bit is 1");
+    swal({
+      icon: "success",
+      title: `1${userInput}` 
+    });
+    //parity bit is 1
+  } else {
+    //parity bit is 0
+  }
+ }
+});
+document.getElementById("submit").addEventListener("click", getOutput);
 
 
 
